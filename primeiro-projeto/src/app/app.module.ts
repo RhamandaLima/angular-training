a7d2e45fadeaf0b46fb015af4417eea9d44a61a3
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-10-20 21:45:00 -0300

    feat: first component

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiro2Component
   ],
   imports: [
     BrowserModule,
