35656b52e6a0b7b3a39fdc0a9063b02cd684e21b
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-12-12 17:06:16 -0300

    feat: mouseover and mouseout

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -16,6 +16,8 @@ export class DataBindingComponent implements OnInit {
   valorAtual: string = '';
   valorSalvo: string = '';
 
+  isMouseOver: boolean = false;
+
   getValor() {
     return 1;
   }
@@ -36,6 +38,10 @@ export class DataBindingComponent implements OnInit {
 
   salvarValor(valor: any) {
     this.valorSalvo = valor;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor() { }
