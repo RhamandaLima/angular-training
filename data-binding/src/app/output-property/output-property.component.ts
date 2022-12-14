500ddb7e21c39d64b961109a4f50474a4411441a
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-12-14 14:02:11 -0300

    feat: Output on decorator

--- data-binding/src/app/output-property/output-property.component.ts
+++ data-binding/src/app/output-property/output-property.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'contador',
   templateUrl: './output-property.component.html',
-  styleUrls: ['./output-property.component.css']
+  styleUrls: ['./output-property.component.css'],
+  outputs: [
+    'mudouValor'
+  ]
 })
 export class OutputPropertyComponent implements OnInit {
 
   @Input() valor: number = 0;
 
-  @Output() mudouValor = new EventEmitter();
+  //@Output()
+  mudouValor = new EventEmitter();
 
   incrementa() {
     this.valor++;
