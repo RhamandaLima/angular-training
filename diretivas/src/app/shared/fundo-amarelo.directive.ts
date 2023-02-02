3afb776889ac9e80b880026fdac97376b4d46040
Rhamanda Lima <cathyannarhamanda@gmail.com>
2023-02-01 21:47:33 -0300

    feat: #goodPractice using Renderer instead of ElementRef

--- diretivas/src/app/shared/fundo-amarelo.directive.ts
+++ diretivas/src/app/shared/fundo-amarelo.directive.ts
@@ -1,13 +1,18 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: 'p[appFundoAmarelo]'
 })
 export class FundoAmareloDirective {
 
-  constructor(private _elementRef: ElementRef) {
+  constructor(private _elementRef: ElementRef,
+    private _renderer: Renderer2) {
     //console.log(this._elementRef);
-    this._elementRef.nativeElement.style.backgroundColor = 'yellow'
+    //this._elementRef.nativeElement.style.backgroundColor = 'yellow'
+    this._renderer.setStyle(
+      this._elementRef.nativeElement,
+      'background-color',
+      'yellow')
    }
 
 }
