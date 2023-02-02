c72f50e62e362546d5ecde951358beef50b7c957
Rhamanda Lima <cathyannarhamanda@gmail.com>
2023-02-01 21:33:32 -0300

    feat: adding background-color yellow with property binding style

--- diretivas/src/app/diretivas-customizadas/diretivas-customizadas.component.ts
+++ diretivas/src/app/diretivas-customizadas/diretivas-customizadas.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-diretivas-customizadas',
   templateUrl: './diretivas-customizadas.component.html',
   styleUrls: ['./diretivas-customizadas.component.scss']
 })
-export class DiretivasCustomizadasComponent implements OnInit {
+export class DiretivasCustomizadasComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
